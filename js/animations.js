0d82718d648fca54a020d1c9209cfc69d05b3da2
Yesh Ramesh <yeshram@gmail.com>
2016-01-06 19:21:01 -0800

    Added rotation animation

--- js/animations.js
+++ js/animations.js
@@ -20,6 +20,26 @@ function blink() {
 
 setTimeout(blink, interval);
 
+function roll() {
+  $("#eye1").pauseKeyframe();
+  $("#eye2").pauseKeyframe();
+  $("#smiley").playKeyframe({
+    name: 'roll',
+    duration: '700ms',
+    timingFunction: 'ease',
+    complete: function(){
+      $("#smiley").resetKeyframe();
+      $("#eye1").resumeKeyframe();
+      $("#eye2").resumeKeyframe();
+    }
+  });
+
+  interval = Math.random() * (15000 - 10000) + 10000;
+  setTimeout(roll, interval);
+}
+
+setTimeout(roll, interval);
+
 $("#eye1").click(function() {
   $("#eye1").playKeyframe({
     name: 'eye1blink',
--- js/keyframes.js
+++ js/keyframes.js
@@ -10,5 +10,13 @@ $.keyframe.define([
     '0%': {'height': '70px', 'margin-top': '60px'},
     '50%': {'height': '2px', 'width': '70px', 'margin-left': '180px', 'margin-top': '90px'},
     '100%': {'height': '70px', 'margin-top': '60px'}
+  },
+  {
+    name: 'roll',
+    '0%': {'transform': 'rotate(0deg)'},
+    '25%': {'transform': 'rotate(40deg)'},
+    '66%': {'transform': 'rotate(-40deg)'},
+    '90%': {'transform': 'rotate(10deg)'},
+    '100%': {'transform': 'rotate(0deg)'}
   }
 ]);
